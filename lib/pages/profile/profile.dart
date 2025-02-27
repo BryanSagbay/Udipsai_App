3746f9c7cca9c0233721e79cfdc02c43c63a8494
Bryan Sagbay <bryansagbay@Bryans-MacBook-Pro.local>
2025-02-27 17:02:52 -0500

    add new animated in profile

--- lib/pages/profile/profile.dart
+++ lib/pages/profile/profile.dart
@@ -21,174 +21,169 @@ class ProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        title: const Text("Profile"),
-      ),
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.symmetric(vertical: 20),
-        child: Column(
-          children: [
-            const ProfilePic(),
-            const SizedBox(height: 20),
-            ProfileMenu(
-              text: "My Account",
-              icon: "lib/icons/gear-wide-connected.svg",
-              press: () => {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => Settings()),
-                )
-              },
-            ),
-            ProfileMenu(
-              text: "Pacientes",
-              icon: "lib/icons/person-rolodex.svg",
-              press: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => PacientesScreen()),
-                );
-              },
-            ),
-            ProfileMenu(
-              text: "Tests",
-              icon: "lib/icons/menu-down.svg",
-              press: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => SeleccionPacientePage(
-                      onPacienteSeleccionado: (pacienteId, pacienteNombre, pacienteApellido) {
-                        print('Redirigiendo a TestPage con paciente: $pacienteNombre $pacienteApellido');
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => TestPage(
-                              pacienteId: pacienteId,
-                              pacienteNombre: pacienteNombre,
-                              pacienteApellido: pacienteApellido,
-                            ),
-                          ),
-                        );
-                      },
-                    ),
-                  ),
-                );
-              },
-            ),
-            ProfileMenu(
-              text: "Soporte TI",
-              icon: "lib/icons/pc-display.svg",
-              press: () {},
-            ),
-            ProfileMenu(
-              text: "Logout",
-              icon: "lib/icons/box-arrow-left.svg",
-              press: () => signUserOut(context),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class ProfilePic extends StatelessWidget {
-  const ProfilePic({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: 115,
-      width: 115,
-      child: Stack(
-        fit: StackFit.expand,
-        clipBehavior: Clip.none,
+      body: Stack(
         children: [
-          const CircleAvatar(
-            backgroundImage:
-            NetworkImage("https://muchomejorecuador.org.ec/wp-content/uploads/2024/04/Universidad-Catolica_de_Cuenca.jpg"),
-          ),
-          Positioned(
-            right: -16,
-            bottom: 0,
-            child: SizedBox(
-              height: 46,
-              width: 46,
-              child: TextButton(
-                style: TextButton.styleFrom(
-                  foregroundColor: Colors.white,
-                  backgroundColor: const Color(0xFFF5F6F9),
-                ),
-                onPressed: () {},
-                child: SvgPicture.string(cameraIcon),
+          // Fondo de la página
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('lib/images/fondo.png'),
+                fit: BoxFit.cover,
               ),
             ),
-          )
+          ),
+
+          // Animación del águila
+          AnimatedPositioned(
+            duration: Duration(seconds: 2),
+            curve: Curves.easeOut,
+            top: 100,
+            left: MediaQuery.of(context).size.width / 2 - 100,
+            child: AnimatedOpacity(
+              duration: Duration(seconds: 2),
+              opacity: 1.0,
+              child: Image.asset(
+                'lib/images/aguila.png',
+                width: 300,
+                height: 300,
+              ),
+            ),
+          ),
+
+          // Contenido principal
+          SingleChildScrollView(
+            padding: const EdgeInsets.symmetric(vertical: 20),
+            child: Column(
+              children: [
+                const SizedBox(height: 300), // Espacio para la animación del águila
+                _buildCarousel(context), // Pasar el contexto aquí
+              ],
+            ),
+          ),
         ],
       ),
     );
   }
-}
 
-class ProfileMenu extends StatelessWidget {
-  const ProfileMenu({
-    Key? key,
-    required this.text,
-    required this.icon,
-    this.press,
-  }) : super(key: key);
-
-  final String text, icon;
-  final VoidCallback? press;
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-      child: TextButton(
-        style: TextButton.styleFrom(
-          foregroundColor: const Color(0xFF930925),
-          padding: const EdgeInsets.all(20),
-          shape:
-          RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
-          backgroundColor: const Color(0xFFF5F6F9),
-        ),
-        onPressed: press,
-        child: Row(
-          children: [
-            SvgPicture.asset(
-              icon,
-              colorFilter:
-              const ColorFilter.mode(Color(0xFF930925), BlendMode.srcIn),
-              width: 22,
+  Widget _buildCarousel(BuildContext context) {
+    return SizedBox(
+      height: 300, // Altura del carrusel
+      child: Column(
+        children: [
+          Expanded(
+            child: PageView.builder(
+              controller: PageController(viewportFraction: 0.3), // Vista previa de los botones adyacentes
+              itemCount: 5, // Número de botones
+              itemBuilder: (context, index) {
+                return _buildCarouselItem(context, index);
+              },
             ),
-            const SizedBox(width: 20),
-            Expanded(
-              child: Text(
-                text,
-                style: const TextStyle(
-                  color: Color(0xFF757575),
-                ),
-              ),
-            ),
-            const Icon(
-              Icons.arrow_forward_ios,
-              color: Color(0xFF757575),
-            ),
-          ],
-        ),
+          ),
+          SizedBox(height: 20),
+        ],
       ),
     );
   }
-}
 
-const cameraIcon =
-'''<svg width="20" height="16" viewBox="0 0 20 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path fill-rule="evenodd" clip-rule="evenodd" d="M10 12.0152C8.49151 12.0152 7.26415 10.8137 7.26415 9.33902C7.26415 7.86342 8.49151 6.6619 10 6.6619C11.5085 6.6619 12.7358 7.86342 12.7358 9.33902C12.7358 10.8137 11.5085 12.0152 10 12.0152ZM10 5.55543C7.86698 5.55543 6.13208 7.25251 6.13208 9.33902C6.13208 11.4246 7.86698 13.1217 10 13.1217C12.133 13.1217 13.8679 11.4246 13.8679 9.33902C13.8679 7.25251 12.133 5.55543 10 5.55543ZM18.8679 13.3967C18.8679 14.2226 18.1811 14.8935 17.3368 14.8935H2.66321C1.81887 14.8935 1.13208 14.2226 1.13208 13.3967V5.42346C1.13208 4.59845 1.81887 3.92664 2.66321 3.92664H4.75C5.42453 3.92664 6.03396 3.50952 6.26604 2.88753L6.81321 1.41746C6.88113 1.23198 7.06415 1.10739 7.26604 1.10739H12.734C12.9358 1.10739 13.1189 1.23198 13.1877 1.41839L13.734 2.88845C13.966 3.50952 14.5755 3.92664 15.25 3.92664H17.3368C18.1811 3.92664 18.8679 4.59845 18.8679 5.42346V13.3967ZM17.3368 2.82016H15.25C15.0491 2.82016 14.867 2.69466 14.7972 2.50917L14.2519 1.04003C14.0217 0.418041 13.4113 0 12.734 0H7.26604C6.58868 0 5.9783 0.418041 5.74906 1.0391L5.20283 2.50825C5.13302 2.69466 4.95094 2.82016 4.75 2.82016H2.66321C1.19434 2.82016 0 3.98846 0 5.42346V13.3967C0 14.8326 1.19434 16 2.66321 16H17.3368C18.8057 16 20 14.8326 20 13.3967V5.42346C20 3.98846 18.8057 2.82016 17.3368 2.82016Z" fill="#757575"/>
-</svg>
-''';
+  Widget _buildCarouselItem(BuildContext context, int index) {
+    final List<Map<String, dynamic>> buttons = [
+      {
+        "icon": "lib/icons/gear-wide-connected.svg",
+        "text": "My Account",
+        "action": () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => Settings()),
+          );
+        },
+      },
+      {
+        "icon": "lib/icons/person-rolodex.svg",
+        "text": "Pacientes",
+        "action": () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => PacientesScreen()),
+          );
+        },
+      },
+      {
+        "icon": "lib/icons/menu-down.svg",
+        "text": "Tests",
+        "action": () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => SeleccionPacientePage(
+                onPacienteSeleccionado: (pacienteId, pacienteNombre, pacienteApellido) {
+                  print('Redirigiendo a TestPage con paciente: $pacienteNombre $pacienteApellido');
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => TestPage(
+                        pacienteId: pacienteId,
+                        pacienteNombre: pacienteNombre,
+                        pacienteApellido: pacienteApellido,
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          );
+        },
+      },
+      {
+        "icon": "lib/icons/pc-display.svg",
+        "text": "Soporte TI",
+        "action": () {},
+      },
+      {
+        "icon": "lib/icons/box-arrow-left.svg",
+        "text": "Logout",
+        "action": () => signUserOut(context),
+      },
+    ];
+
+    final button = buttons[index];
+    return GestureDetector(
+      onTap: button["action"],
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SvgPicture.asset(
+            button["icon"],
+            width: 50,
+            height: 50,
+          ),
+          SizedBox(height: 10),
+          Text(
+            button["text"],
+            style: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+              color: Colors.white, // Asegura que el texto sea visible sobre cualquier fondo
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildPageIndicator() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: List.generate(5, (index) {
+        return Container(
+          width: 8,
+          height: 8,
+          margin: EdgeInsets.symmetric(horizontal: 4),
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: index == 0 ? Colors.blue : Colors.grey, // Cambia el color según la página activa
+          ),
+        );
+      }),
+    );
+  }
+}
