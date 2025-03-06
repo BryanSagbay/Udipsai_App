82a5faaf68063ae0c8caf9ae086504abf60f4f9e
Bryan Sagbay <bryansagbay@Bryans-MacBook-Pro.local>
2025-03-06 08:16:54 -0500

    edit select

--- lib/pages/paciente/selectPaciente/selectPacientes.dart
+++ lib/pages/paciente/selectPaciente/selectPacientes.dart
@@ -22,7 +22,8 @@ class _SeleccionPacientePageState extends State<SeleccionPacientePage> {
         children: [
           Positioned.fill(
             child: Image.asset(
-              'lib/images/ofertafond.jpg', // Asegúrate de que esta imagen exista
+              'lib/images/ofertafond.jpg',
+              // Asegúrate de que esta imagen exista
               fit: BoxFit.cover,
             ),
           ),
@@ -40,7 +41,8 @@ class _SeleccionPacientePageState extends State<SeleccionPacientePage> {
       onTap: () {
         // Si se toca fuera del modal, redirigimos a ProfileScreen
         Navigator.of(context).pop(); // Cerrar el modal
-        Navigator.of(context).pushReplacement( // Usamos pushReplacement para que no se quede en el stack
+        Navigator.of(context)
+            .pushReplacement( // Usamos pushReplacement para que no se quede en el stack
           MaterialPageRoute(builder: (context) => ProfileScreen()),
         );
       },
@@ -54,52 +56,66 @@ class _SeleccionPacientePageState extends State<SeleccionPacientePage> {
           child: Column(
             mainAxisSize: MainAxisSize.min,
             children: [
-              Text('Selecciona un Paciente', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+              Text('Selecciona un Paciente',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
               Divider(),
-              StreamBuilder<QuerySnapshot>(
-                stream: FirebaseFirestore.instance.collection('pacientes').snapshots(),
-                builder: (context, snapshot) {
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return Center(child: CircularProgressIndicator());
-                  }
-                  if (snapshot.hasError) {
-                    return Center(child: Text('Error al cargar pacientes'));
-                  }
-                  if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
-                    return Center(child: Text('No hay pacientes registrados.'));
-                  }
+              Expanded(
+                child: StreamBuilder<QuerySnapshot>(
+                  stream: FirebaseFirestore.instance.collection('pacientes')
+                      .snapshots(),
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return Center(child: CircularProgressIndicator());
+                    }
+                    if (snapshot.hasError) {
+                      return Center(child: Text('Error al cargar pacientes'));
+                    }
+                    if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+                      return Center(
+                          child: Text('No hay pacientes registrados.'));
+                    }
 
-                  final pacientes = snapshot.data!.docs;
-                  return ListView.builder(
-                    shrinkWrap: true, // Para que el ListView ocupe solo el espacio necesario
-                    itemCount: pacientes.length,
-                    itemBuilder: (context, index) {
-                      final paciente = pacientes[index].data() as Map<String, dynamic>;
-                      final pacienteId = pacientes[index].id;
-                      final pacienteNombre = paciente['nombre'] ?? 'Sin Nombre';
-                      final pacienteApellido = paciente['apellido'] ?? '';
+                    final pacientes = snapshot.data!.docs;
+                    return ListView.builder(
+                      shrinkWrap: true,
+                      // Para que el ListView ocupe solo el espacio necesario
+                      itemCount: pacientes.length,
+                      itemBuilder: (context, index) {
+                        final paciente = pacientes[index].data() as Map<
+                            String,
+                            dynamic>;
+                        final pacienteId = pacientes[index].id;
+                        final pacienteNombre = paciente['nombre'] ??
+                            'Sin Nombre';
+                        final pacienteApellido = paciente['apellido'] ?? '';
 
-                      return ListTile(
-                        title: Text('$pacienteNombre $pacienteApellido'),
-                        onTap: () {
-                          // Llamamos al método para pasar los datos al padre
-                          widget.onPacienteSeleccionado(pacienteId, pacienteNombre, pacienteApellido);
-                          setState(() {
-                            _modalVisible = false; // Ocultar el modal
-                          });
-                          Navigator.of(context).pop(); // Cerrar el modal
-                          Navigator.pushReplacement(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => TestPage(pacienteId: pacienteId, pacienteNombre: pacienteNombre, pacienteApellido: pacienteApellido),
-                            ),
-                          );
-                        },
-                      );
-                    },
-                  );
-                },
+                        return ListTile(
+                          title: Text('$pacienteNombre $pacienteApellido'),
+                          onTap: () {
+                            // Llamamos al método para pasar los datos al padre
+                            widget.onPacienteSeleccionado(
+                                pacienteId, pacienteNombre, pacienteApellido);
+                            setState(() {
+                              _modalVisible = false; // Ocultar el modal
+                            });
+                            Navigator.of(context).pop(); // Cerrar el modal
+                            Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => TestPage(
+                                    pacienteId: pacienteId,
+                                    pacienteNombre: pacienteNombre,
+                                    pacienteApellido: pacienteApellido),
+                              ),
+                            );
+                          },
+                        );
+                      },
+                    );
+                  },
+                ),
               ),
+              // El botón cancelar se mantiene fijo en la parte inferior
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
@@ -107,8 +123,10 @@ class _SeleccionPacientePageState extends State<SeleccionPacientePage> {
                     onPressed: () {
                       // Aquí redirigimos directamente a ProfileScreen sin usar pop() para evitar el fondo negro
                       Navigator.of(context).pop(); // Cerrar el modal
-                      Navigator.of(context).pushReplacement( // Usamos pushReplacement para evitar el fondo negro
-                        MaterialPageRoute(builder: (context) => ProfileScreen()),
+                      Navigator.of(context)
+                          .pushReplacement( // Usamos pushReplacement para evitar el fondo negro
+                        MaterialPageRoute(
+                            builder: (context) => ProfileScreen()),
                       );
                     },
                     child: Text('Cancelar'),
